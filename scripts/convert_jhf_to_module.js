dab3b2ff8f49588b827927e5ca646d1072925317
Ben Nortier <ben@bjnortier.com>
2019-01-16 11:03:42 +0200

    2.1.1

--- scripts/convert_jhf_to_module.js
+++ scripts/convert_jhf_to_module.js
@@ -6,7 +6,7 @@ var jhfFilenames = fs.readdirSync('font/jhf').filter(function (filename) {
 jhfFilenames.forEach(function (sourceFilename) {
   // Read as base64 and write back as a module
   const base64 = fs.readFileSync('font/jhf/' + sourceFilename, 'base64')
-  const module = 'export default "' + base64 + '";'
+  const module = 'export default `' + base64 + '`\n'
   try { fs.mkdirSync('font/js') } catch (e) {}
   fs.writeFileSync('src/' + /^(.*)\.jhf$/.exec(sourceFilename)[1] + '.js', module)
 })
